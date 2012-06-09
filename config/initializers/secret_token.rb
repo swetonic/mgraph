0e399db85a8c8de2439c4d18be20b6383dd5c207
Tim Swetonic <swetonic@gmail.com>
2012-06-09 13:28:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicianGraph::Application.config.secret_token = '220f2a04d00838549b3136d16015a602e5aeb60080f67d64594d6258b59d5a56e56b8a75e84beecec8666c856237d7fe6662b26f3c5fd9efa4931152b55cde6a'
---
+++ test/unit/helpers/data_manager_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DataManagerHelperTest < ActionView::TestCase
+end
